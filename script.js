55ad2d1cf65bec8c279619c258aa4053efa635a0
julien77420 <bahdhyga1@gmail.com>
2026-08-15 00:40:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ function showPage(pageId) {
 
     document.getElementById(pageId).classList.add("active");
 }
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker.register("sw.js");
+    });
+}
+const foodCheckboxes = document.querySelectorAll(".food input");
+
+foodCheckboxes.forEach((checkbox, index) => {
+
+    const saved = localStorage.getItem("massup_food_" + index);
+
+    if (saved === "true") {
+        checkbox.checked = true;
+    }
+
+    checkbox.addEventListener("change", function() {
+        localStorage.setItem(
+            "massup_food_" + index,
+            checkbox.checked
+        );
+    });
+});
+function showShoppingList() {
+    const foods = document.querySelectorAll(".food");
+    const shoppingList = document.getElementById("shoppingList");
+
+    shoppingList.innerHTML = "";
+
+    foods.forEach(function(food) {
+        const checkbox = food.querySelector("input");
+
+        if (checkbox.checked) {
+            const item = document.createElement("p");
+            item.textContent = "🛒 " + food.textContent.trim();
+            shoppingList.appendChild(item);
+        }
+    });
+
+    if (shoppingList.innerHTML === "") {
+        shoppingList.innerHTML = "<p>Aucun aliment sélectionné.</p>";
+    }
+}
